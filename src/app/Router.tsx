1a6d95d4c1879f881e44c1c9c01b45370d07a380
katua <katua.logvinova@gmail.com>
2024-12-01 10:38:57 +0300

    fix router

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { StructurePage, UserInfoPage } from '../pages';
 import { StoreProvider } from './StoreContext';
 
